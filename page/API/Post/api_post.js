948218262fffee43c06528d616a428fec19f7800
liyuning1985 <liyuning1985@163.com>
2017-01-17 18:52:54 +0800

    Create api_post.js

---
+++ page/API/Post/api_post.js
@@ -0,0 +1,38 @@
+define(['Ly'],function(Ly) {
+	var Ctrl = ['$rootScope','$scope'];
+	var CtrlFn = function($rootScope,$scope) {
+
+		var id = $rootScope.TopTabs.TabSelected;
+		$scope.root = {
+			title:""
+		}
+
+
+		$scope.$watch("root",function(newvalue,oldvalue) {
+			var CacheList = $rootScope.Cache.List;
+			var scope = {}
+			var id = $rootScope.TopTabs.TabSelected;
+			scope.id = id;
+			scope.root = newvalue;
+			if(!Ly.findObj("id",scope.id,CacheList)){
+				CacheList.push(scope);
+			}else{
+				var cache = Ly.findObj("id",scope.id,CacheList);
+				cache.root = newvalue;
+			}
+		},true);
+		init(id);
+		function init(id) {
+			var CacheList = angular.copy($rootScope.Cache.List);
+			var cache = Ly.findObj("id",id,CacheList);
+			if(cache) {
+				var root = cache.root;
+				for(var key in root){
+					$scope.root[key] = root[key];
+				}
+			}
+		}
+	}
+	Ctrl.push(CtrlFn);
+	return Ctrl;
+});
