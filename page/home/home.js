7b397e64165870c73c139c3542dcb25991f7a3b9
liyuning1985 <liyuning1985@163.com>
2017-01-22 17:38:21 +0800

    Update home.js

--- page/home/home.js
+++ page/home/home.js
@@ -1,13 +1,41 @@
-define(['Ly'],function(Ly){
-	var Ctrl = ['$scope'];
-	var CtrlFn = function($scope) {	
-		$scope.UI = {
-			nowDate:function(){
-				var date = new Date();
-				return date.format("yyyy-MM-dd");
-			}
-		}
-	}
-	Ctrl.push(CtrlFn);
-	return Ctrl;
+define([
+    "framework/common"
+], function (Ly) {
+    var Ctrl = ["$rootScope", "$scope"];
+    var Fn = function ($rootScope, $scope) {
+        var id = $rootScope.TopTabs.TabSelected;
+        $scope.root = {
+            title: ""
+
+        }
+        console.info($scope);
+
+        $scope.$watch("root", function (newvalue, oldvalue) {
+            var CacheList = $rootScope.Cache.List;
+            console.info($rootScope.TopTabs.TabSelected);
+            var scope = {}
+            var id = $rootScope.TopTabs.TabSelected;
+            scope.id = id;
+            scope.root = newvalue;
+            if (!Ly.findObj("id", scope.id, CacheList)) {
+                CacheList.push(scope);
+            } else {
+                var cache = Ly.findObj("id", scope.id, CacheList);
+                cache.root = newvalue;
+            }
+        }, true);
+        init(id);
+        function init(id) {
+            var CacheList = angular.copy($rootScope.Cache.List);
+            var cache = Ly.findObj("id", id, CacheList);
+            if (cache) {
+                var root = cache.root;
+                for (var key in root) {
+                    $scope.root[key] = root[key];
+                }
+            }
+        }
+    };
+    Ctrl.push(Fn);
+    return Ctrl;
 });
