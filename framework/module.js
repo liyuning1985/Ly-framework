c7e99159a54d8dcaf6877aa5ef70ab7318be4b0b
liyuning1985 <liyuning1985@163.com>
2017-01-26 15:32:28 +0800

    Update module.js

--- framework/module.js
+++ framework/module.js
@@ -2,7 +2,8 @@ define(function (require) {
     "use strict";
     var mod = [
         require("portal/config"),
-        require("business/System/configures/SystemConfig")
+        require("business/System/configures/SystemConfig"),
+        require("business/Msys/configures/msysConfig")
     ]
     for (var i in mod) {
         var func = mod[i];
