8c7d50e2ea7de21d4fd4333a80a3640aa9daf538
liyuning1985 <liyuning1985@163.com>
2017-01-26 15:35:05 +0800

    Update RoleListCtrl.js

--- business/System/controllers/RoleListCtrl.js
+++ business/System/controllers/RoleListCtrl.js
@@ -8,6 +8,34 @@ define([
             title: ""
 
         }
+
+        $('#jstree1').jstree({
+            'core': {
+                'check_callback': true
+            },
+            'plugins': ['types', 'dnd'],
+            'types': {
+                'default': {
+                    'icon': 'fa fa-folder'
+                },
+                'html': {
+                    'icon': 'fa fa-file-code-o'
+                },
+                'svg': {
+                    'icon': 'fa fa-file-picture-o'
+                },
+                'css': {
+                    'icon': 'fa fa-file-code-o'
+                },
+                'img': {
+                    'icon': 'fa fa-file-image-o'
+                },
+                'js': {
+                    'icon': 'fa fa-file-text-o'
+                }
+
+            }
+        });
         $scope.$watch("root", function (newvalue, oldvalue) {
             var CacheList = $rootScope.Cache.List;
             console.info($rootScope.TopTabs.TabSelected);
