d47d7f7f41465069f68fc6bb058d416583f22e13
liyuning1985 <liyuning1985@163.com>
2018-02-14 11:41:17 +0800

    22
    22

--- framework/framework.js
+++ framework/framework.js
@@ -12,5 +12,12 @@ function () {
     ];
 
     var framework = angular.module("framework", dependency);
+	 framework.config(function($stateProvider, $controllerProvider) {
+        framework.register = {
+            "stateProvider": $stateProvider,
+            "controllerProvider": $controllerProvider
+        }
+    })
+	
     return framework;
 });
